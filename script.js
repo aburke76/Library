8c9273f6c35a590fe912064b481e3f7f8cdcf7ce
aburke76 <alexlburke8771@gmail.com>
2024-03-02 16:31:10 -0500

    created modal and form

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const cardContainer = document.querySelector(".card-container");
+const card = document.createElement("div");
+
+const modal = document.querySelector(".modal");
+const addBookBtn = document.querySelector(".book-btn");
+const closeBtn = document.querySelector(".close");
+
+addBookBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
 const myLibrary = [
-    {
-        title: "Hair",
-        author: "Leslie Patricelli",
-        pages: 26,
-        read: "Read",
-    },
-    {
-        title: "No No Yes Yes",
-        author: "Leslie Patricelli",
-        pages: 26,
-        read: "Read",
-    },
     {
         title: "The Restaraunt at the End of the Universe",
         author: "Douglas Adams",
@@ -19,8 +28,6 @@ const myLibrary = [
     },
 ];
 
-console.log(myLibrary);
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -28,4 +35,32 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+    myLibrary.forEach(() => {
+        card.className = "card";
+        cardContainer.append(card);
+    });
+    addInfoToBook();
+}
+
+function addInfoToBook() {
+    let list = document.createElement("ul");
+    let listTitle = document.createElement("li");
+    listTitle.className = "title";
+    let listAuthor = document.createElement("li");
+    listAuthor.className = "author";
+    let listPages = document.createElement("li");
+    listPages.className = "pages";
+    let listRead = document.createElement("li");
+    listRead.className = "read";
+    let listDelete = document.createElement("li");
+    listDelete.className = "delete";
+    list.append(listTitle);
+    list.append(listAuthor);
+    list.append(listPages);
+    list.append(listRead);
+    list.append(listDelete);
+    card.append(list);
+}
+
+addBookToLibrary();
